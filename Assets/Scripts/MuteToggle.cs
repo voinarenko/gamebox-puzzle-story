1d5b26c54fe14878551c947ddc71219cb2dd5d91
Sergei Voinarenko <voynarenko@gmail.com>
2023-06-05 13:45:59 +0300

    Audio track added, mute button added.

---
+++ Assets/Scripts/MuteToggle.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Toggle))]
+    public class MuteToggle : MonoBehaviour
+    {
+        // Выключатель звука
+
+        [SerializeField] private Sprite[] _images;      // массив изображений
+        private Toggle _toggle;
+
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
+        private void Start()
+        {
+            GetComponent<Image>().sprite = _images[1];
+            _toggle = GetComponent<Toggle>();
+            _toggle.isOn = true;
+            if (AudioListener.volume == 0)
+            {
+                _toggle.isOn = false;
+                GetComponent<Image>().sprite = _images[0];
+            }
+        }
+
+        /// <summary>
+        /// Метод выключения звука
+        /// </summary>
+        /// <param name="audioIn"></param>
+        public void ToggleAudioOnValueChange(bool audioIn)
+        {
+            if (audioIn)
+            {
+                AudioListener.volume = 1;
+                GetComponent<Image>().sprite = _images[1];
+            }
+            else
+            {
+                AudioListener.volume = 0;
+                GetComponent<Image>().sprite = _images[0];
+            }
+        }
+    }
+}
