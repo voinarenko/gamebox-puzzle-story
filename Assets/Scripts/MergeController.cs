47f5b9bdef90add74166aa94b6ea361dc026f595
Sergei Voinarenko <voynarenko@gmail.com>
2023-06-06 15:25:08 +0300

    Spell dragging added.

--- Assets/Scripts/MergeController.cs
+++ Assets/Scripts/MergeController.cs
@@ -1,13 +1,23 @@
+using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Assets.Scripts
 {
-    public class MergeController : MonoBehaviour, IPointerClickHandler, IDragHandler
+    public class MergeController : EventTrigger
     {
         // Управление слияниями
 
-        private SpellGenerator _spellGenerator;                                                    // Генератор заклинаний
+
+        #region Переменные
+
+        private SpellGenerator _spellGenerator;                                                     // Генератор заклинаний
+        private bool _dragging;                                                                     // состояние перетаскивания
+        private float _offsetX, _offsetY;                                                           // смещение
+        private Vector3 _startPosition;                                                             // начальные координаты
+
+        #endregion
 
         /// <summary>
         /// Start is called before the first frame update
@@ -15,27 +25,47 @@ namespace Assets.Scripts
         private void Start()
         {
             _spellGenerator = FindObjectOfType(typeof(SpellGenerator)) as SpellGenerator;
+        }        
+        
+        /// <summary>
+        /// Метод обработки щелчка по левой клавише мыши
+        /// </summary>
+        /// <param name="eventData"></param>
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            if (!_spellGenerator.IsAnimating && !_dragging) ClickMerge();
         }
 
         /// <summary>
-        /// Метод обработки нажатия на левую клавишу мыши
+        /// Метод обработки длительного нажатия на левую клавишу мыши
         /// </summary>
         /// <param name="eventData"></param>
-        public void OnPointerClick(PointerEventData eventData)
+        public override void OnPointerDown(PointerEventData eventData)
         {
-            if (!_spellGenerator.IsAnimating) ClickMerge();
+            _startPosition = transform.position;
+            _offsetX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+            _offsetY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
         }
 
         /// <summary>
         /// Метод обработки перетаскивания
         /// </summary>
         /// <param name="eventData"></param>
-        /// <exception cref="System.NotImplementedException"></exception>
-        public void OnDrag(PointerEventData eventData)
+        public override void OnDrag(PointerEventData eventData)
         {
-            throw new System.NotImplementedException();
+            _dragging = true;
+            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mousePosition.x - _offsetX, mousePosition.y - _offsetY, 90);
         }
-        
+
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            if (!_dragging) return;            
+            transform.DOMove(_startPosition, 1f).SetEase(Ease.OutBounce).OnComplete(() =>
+            {
+                _dragging = false;
+            });
+        }
 
         /// <summary>
         /// Метод обработки слияния объектов
